1083adb24defbfa558a134c1b763f6d21bce6cb9
diorgelesdias <diorgelesdias@gmail.com>
2020-05-10 17:03:42 -0300

    fix ORMconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,15 +1,21 @@
-console.log(process.env.DATABASE_URL);
-
+let config = {};
+if (process.env.NODE_ENV === 'development') {
+  config = {
+    host: process.env.DATABASE_URL,
+    extra: {
+      socketPath: process.env.DATABASE_URL,
+    },
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  };
+} else {
+  config = { url: process.env.DATABASE_URL };
+}
 module.exports = {
   type: 'postgres',
-  host: process.env.DATABASE_URL,
-  extra: {
-    socketPath: process.env.DATABASE_URL,
-  },
-  port: 5432,
-  database: process.env.POSTGRES_DB,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+  ...config,
   entities: [__dirname + '/**/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   migrationsRun: true,
