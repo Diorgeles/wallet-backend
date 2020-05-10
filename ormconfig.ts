ad31091c8cd16f76eaf8f0809e7fefc3423ee639
diorgelesdias <diorgelesdias@gmail.com>
2020-05-10 17:12:50 -0300

    orm

--- ormconfig.ts
+++ ormconfig.ts
@@ -9,15 +9,20 @@ if (process.env.NODE_ENV === 'development') {
     database: process.env.POSTGRES_DB,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
+    entities: [__dirname + '/**/**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/migrations/*{.ts,.js}'],
   };
 } else {
-  config = { url: process.env.DATABASE_URL };
+  config = {
+    url: process.env.DATABASE_URL,
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/migrations/*{.ts,.js}'],
+  };
 }
 module.exports = {
   type: 'postgres',
   ...config,
-  entities: [__dirname + '/**/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+
   migrationsRun: true,
   logging: true,
   logger: 'advanced-console',
